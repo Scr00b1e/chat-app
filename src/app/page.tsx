5f27226d85397daff7adfca3633a3b67f64362d5
Jack <kbolot312@gmail.com>
2023-02-06 22:08:44 +0600

    login component ui done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+'use client'
+
 import Login from "@/components/Login";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <div>
+    <ChakraProvider>
       <Login />
-    </div>
+    </ChakraProvider>
   )
 }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,13 +1,21 @@
 'use client'
 
 import { ChatIcon } from '@chakra-ui/icons'
+import { Box, Button, Center, Stack } from '@chakra-ui/react'
 import React from 'react'
 
 const Login = () => {
     return (
-        <div>
-            <ChatIcon />
-        </div>
+        <Center h={'100vh'}>
+            <Stack align={'center'} bgColor='gray.600' p={16} rounded={'3xl'} spacing={12} boxShadow='1g'>
+                <Box bgColor={'blue.500'} w='fit-content' p={5} rounded={'3xl'} boxShadow={'md'}>
+                    <ChatIcon w={100} h={100} />
+                </Box>
+                <Button boxShadow={'md'}>
+                    Sign in with Google
+                </Button>
+            </Stack>
+        </Center>
     )
 }
 
