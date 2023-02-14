db841f5132f7b2d01b2e75186dc0e56196b150bd
Jack <kbolot312@gmail.com>
2023-02-14 20:39:18 +0600

    sidebar basicly done

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,12 +9,13 @@ import { signOut } from 'firebase/auth'
 import { auth, db } from 'firebaseconfig'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { useCollection } from 'react-firebase-hooks/firestore'
-import { collection } from 'firebase/firestore'
+import { addDoc, collection } from 'firebase/firestore'
+import { getOtherEmail } from 'utils/getOtherEmail'
 
 const Sidebar = () => {
     const router = useRouter()
     const [user] = useAuthState(auth);
-    const [snapshot, loading, error] = useCollection(collection(db, 'chats'))
+    const [snapshot] = useCollection(collection(db, 'chats'))
     const chats = snapshot?.docs.map(obj => ({ id: obj.id, ...obj.data() }))
 
     const onSignOut = () => {
@@ -22,7 +23,14 @@ const Sidebar = () => {
         signOut(auth)
     }
 
+    const chatExits = email => chats?.find(chat => (chat.users.includes(user?.email) && chat.users.includes(email)))
 
+    const newChat = async () => {
+        const input = prompt('Enter email')
+        if (!chatExits(input) && (input != user?.email)) {
+            await addDoc(collection(db, 'chats'), { users: [user?.email, input] })
+        }
+    }
 
     return (
         <Flex w={'300px'}
@@ -37,17 +45,16 @@ const Sidebar = () => {
                 <IconButton size={'sm'} isRound icon={<ArrowLeftIcon />} aria-label={''} onClick={onSignOut} />
             </Flex>
 
-            <Button marginBottom={5}>New Chat</Button>
+            <Button marginBottom={5} onClick={newChat}>New Chat</Button>
 
             <Flex overflowY={'scroll'} direction='column' sx={{ scrollbarWidth: "none" }} flex={1}>
-                {/* <Chat /> */}
                 {
                     chats?.map(chat => (
                         <Flex key={Math.random()} p={3} _hover={{ bg: 'gray.100' }} cursor='pointer' align={'center'}
                             onClick={() => router.push(`/chat/${chat.id}`)}>
                             <Avatar src='' marginEnd={3} />
-                            {/* <Text>{getOtherEmail(chat.users, user)}</Text> */}
-                            <Text>{chat.users}</Text>
+                            <Text>{getOtherEmail(chat.users, user)}</Text>
+                            {/* <Text>{chat.users}</Text> */}
                         </Flex>
                     ))
                 }
