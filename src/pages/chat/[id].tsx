cc7b3db368044e5f87d3c8256adb77e1753e0602
Jack <kbolot312@gmail.com>
2023-02-15 21:47:10 +0600

    show user email

--- src/pages/chat/[id].tsx
+++ src/pages/chat/[id].tsx
@@ -1,16 +1,18 @@
 import Sidebar from '@/components/Sidebar'
 import { Avatar, Button, ChakraProvider, Flex, FormControl, Heading, Input, Text } from '@chakra-ui/react'
-import { collection, orderBy, query } from 'firebase/firestore'
-import { db } from 'firebaseconfig'
+import { collection, doc, orderBy, query } from 'firebase/firestore'
+import { auth, db } from 'firebaseconfig'
 import { useRouter } from 'next/router'
 import React from 'react'
-import { useCollectionData } from 'react-firebase-hooks/firestore'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData, useDocumentData } from 'react-firebase-hooks/firestore'
+import { getOtherEmail } from 'utils/getOtherEmail'
 
-const TopBar = () => {
+const TopBar = ({ email }) => {
     return (
         <Flex bg={'gray.100'} h='80px' w={'100%'} align='center' p={5}>
             <Avatar marginEnd={3} />
-            <Heading size={'lg'}>SomeFella@gmail.com</Heading>
+            <Heading size={'lg'}>{email}</Heading>
         </Flex>
     )
 }
@@ -27,10 +29,24 @@ const BottomBar = () => {
 const ChatId = () => {
     const router = useRouter()
     const { id } = router.query
+    const [user] = useAuthState(auth)
+    const [chat] = useDocumentData(doc(db, 'chats', id))
+    console.log(chat);
 
     const q = query(collection(db, `chats/${id}/messages`), orderBy('timestamp'))
     const [messages] = useCollectionData(q)
-    console.log(messages);
+
+    const getMessages = () =>
+        messages?.map(msg => {
+            const sender = msg.sender === user?.email
+            return (
+                <Flex key={Math.random()} bg={sender ? 'green.200' : 'gray.100'} alignSelf={sender ? 'flex-end' : 'flex-start'}
+                    w='fit-content' minWidth={'100px'} borderRadius='lg' p={3} m={1}>
+                    <Text>{msg.text}</Text>
+                    {/* <Text>{msg.timestamp}</Text> */}
+                </Flex>
+            )
+        })
 
     return (
         <ChakraProvider>
@@ -38,15 +54,9 @@ const ChatId = () => {
                 <Sidebar />
                 <Flex h={'100vh'} width='100%'>
                     <Flex bg={'blue.100'} flex={1} direction='column'>
-                        <TopBar />
+                        <TopBar email={getOtherEmail(chat?.users, user)} />
                         <Flex flex={1} direction='column' pt={4} mx={5}>
-                            {
-                                messages?.map(msg => (
-                                    <Flex key={Math.random()} bg={'green.200'} w='fit-content' minWidth={'100px'} borderRadius='lg' p={3} m={1}>
-                                        <Text>{msg.text}</Text>
-                                    </Flex>
-                                ))
-                            }
+                            {getMessages()}
                         </Flex>
                         <BottomBar />
                     </Flex>
