55a5e7f2888394b95ac44b175851dce3d487e9a0
Jack <kbolot312@gmail.com>
2023-02-16 22:11:55 +0600

    smooth scroll added:

--- src/pages/chat/[id].tsx
+++ src/pages/chat/[id].tsx
@@ -8,17 +8,18 @@ import { useAuthState } from 'react-firebase-hooks/auth'
 import { useCollectionData, useDocumentData } from 'react-firebase-hooks/firestore'
 import { getOtherEmail } from 'utils/getOtherEmail'
 
-const TopBar = ({ email }) => {
-    return (
-        <Flex bg={'gray.100'} h='80px' w={'100%'} align='center' p={5}>
-            <Avatar marginEnd={3} />
-            <Heading size={'lg'}>{email}</Heading>
-        </Flex>
-    )
-}
+// const TopBar = () => {
+//     return (
+//         <Flex bg={'gray.100'} h='80px' w={'100%'} align='center' p={5}>
+//             <Avatar marginEnd={3} />
+//             <Heading size={'lg'}>{email}</Heading>
+//         </Flex>
+//     )
+// }
 
 const ChatId = () => {
     const router = useRouter()
+    const bottomChat = React.useRef()
     const { id } = router.query
     const [user] = useAuthState(auth)
     // const [chat] = useDocumentData(doc(db, "chats", id))
@@ -60,6 +61,15 @@ const ChatId = () => {
             )
         })
 
+    React.useEffect(() => {
+        setTimeout(
+            bottomChat.current?.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            }), 100
+        )
+    }, [messages])
+
     return (
         <ChakraProvider>
             <Flex>
@@ -69,6 +79,7 @@ const ChatId = () => {
                         {/* <TopBar email={getOtherEmail(chat?.users, user)} /> */}
                         <Flex flex={1} direction='column' pt={4} mx={5}>
                             {getMessages()}
+                            <div ref={bottomChat}></div>
                         </Flex>
                         <BottomBar />
                     </Flex>
